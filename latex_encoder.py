dd3833699cf411e5f309d0d61cb07ee17de70f5c
sion-k <ygonepiece@naver.com>
2023-07-10 13:21:54 +0900

    latex encoder using plt

--- latex_encoder.py
+++ latex_encoder.py
@@ -1,6 +1,13 @@
 from latex2svg import latex2svg
 import base64
 import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+import io
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Helvetica"
+})
 
 
 def encode(latex: str):
@@ -8,8 +15,7 @@ def encode(latex: str):
     Encodes a LaTeX into svg
     Encodes a svg into base64.
     """
-    svg = latex2svg(f"\({latex}\)")['svg']
-    svg = color_white(svg)
+    svg = latex_to_svg(latex)
     return svg2base64(svg)
 
 
@@ -33,3 +39,35 @@ def svg2base64(svg_code: str):
     base64_string = base64_encoded.decode('utf-8')
 
     return base64_string
+
+
+def latex_to_svg(latex_code, font_size=12):
+    # Calculate height and width based on font size
+    height = font_size / 72  # Convert font size from points to inches
+    # Assume average character width of font_size/2
+    width = len(latex_code) * (font_size / 2) / 72
+
+    fig, ax = plt.subplots(figsize=(width, height))
+    ax.text(0.5, 0.5, f'${latex_code}$', fontsize=font_size,
+            ha='center', va='center', transform=ax.transAxes, color='white')
+
+    # Remove margins and borders
+    ax.margins(0)
+    ax.set_axis_off()
+
+    # Save the plot as SVG
+    svg_buffer = io.BytesIO()
+    plt.savefig(svg_buffer, format='svg', transparent=True,
+                bbox_inches='tight', pad_inches=0)
+    plt.close(fig)
+
+    # Retrieve the SVG code
+    svg_code = svg_buffer.getvalue().decode()
+
+    return svg_code
+
+
+# Example usage
+# latex_equation = r'\frac{{d}}{{dx}}(x^2) = 2x'
+# svg_equation = latex_to_svg(latex_equation)
+# print(svg_equation)
